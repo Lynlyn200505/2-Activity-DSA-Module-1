e5bf8f2777292bc429d8a048bd002578e9d2701b
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-09-10 08:12:19 +0800

    Create Variable.js

---
+++ Variable.js
@@ -0,0 +1,9 @@
+let name = "Ailyn L. Gasatan";
+let whatDoYouWannaBecomeinYourlife = "programmer";
+let gender = "female";
+let twitterHandle = "@GasatanAilyn";
+
+console.log('My name is ' , name);
+console.log('I want to become a ' , whatDoYouWannaBecomeinYourlife);
+console.log('My gender is ' , gender);
+console.log('My twitter handle is ' , twitterHandle);
